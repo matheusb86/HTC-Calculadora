77bc97341382d52b5131a6748f8fe17c16667bf1
Matheus <matheusb86@gmail.com>
2022-05-23 10:37:48 -0300

    calculadora com as funcionalidades

--- js/index.js
+++ js/index.js
@@ -3,8 +3,30 @@ const $buttonsOperators = document.querySelectorAll(".calculator__buttons--opera
 const $inputGetNumbers = document.querySelector(".calculator__display-input")
 const $result = document.querySelector(".calculator__display-result")
 const $buttonResult =document.querySelector(".calculator__buttons--button--result")
+const $clear = document.querySelector(".calculator__buttons--button-c")
+const $negative = document.querySelector('.calculator__buttons--negative')
 
 let calculate = ""
+
+$result.addEventListener('click', function (){    
+    $inputGetNumbers.value = $inputGetNumbers.value.replace("÷","/")
+    $inputGetNumbers.value = $inputGetNumbers.value.replace("X","*")  
+    $inputGetNumbers.value = $inputGetNumbers.value.replace("%","/100")
+    calculate = $inputGetNumbers.value
+    $result.innerHTML = eval($inputGetNumbers.value)
+})
+
+$negative.addEventListener('click',function (){    
+    
+    let verifyNegative = $inputGetNumbers.value.includes("-") 
+
+    if(verifyNegative == false){  
+        $inputGetNumbers.value = $inputGetNumbers.value.replace(" ","") 
+        $inputGetNumbers.value = "-"+$inputGetNumbers.value
+    }else{
+        $inputGetNumbers.value = $inputGetNumbers.value.replace("-","") 
+    }
+})
 
 $buttonsNumbers.forEach(function($button){
     $button.addEventListener("click", function(){
@@ -23,4 +45,11 @@ $buttonsOperators.forEach(function($button){
 $buttonResult.addEventListener("click", function(){
     console.log(calculate)
     $result.textContent = eval(calculate)
+})
+
+$clear.addEventListener("click", function(){
+    $result.innerHTML = ""
+    $inputGetNumbers.value = ''
+    $result.innerHTML = "0"
+
 })
